902a779578612efdb51db2437fe33a9955b411be
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-05 12:06:24 -0400

    added data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  Trip.create(title: "Visit Nil Place", user_id: 1)
+  Objective.create(title: "Go to statue of lib", trip_id: i)
+end
