4f6b29e96d3d9dff08605a2531db31fd3cc04411
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-05 14:39:27 -0400

    added validation on create to geolocate locations

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,5 +1,18 @@
 class Objective < ActiveRecord::Base
 
   belongs_to :trip
-  acts_as_mappable :auto_geocode=>{:field=>:city, :error_message=>'Could not geocode address'}
+  acts_as_mappable
+  before_validation :geocode_location, :on => :create
+
+  private
+
+    def geocode_location
+      geo=Geokit::Geocoders::MultiGeocoder.geocode (self.to_addr)
+      errors.add(:address, "Could not Geocode address") if !geo.success
+      self.lat, self.lng = geo.lat,geo.lng if geo.success
+    end
+
+    def to_addr
+      self.city + self.state + self.country
+    end
 end
