ae6f594bc2913cbdc1873131749bf63b58a83b8b
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-09 12:43:16 -0400

    added concatenation to convert attributes to address, and set lat and lng fields in geocode_location method

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,23 +1,18 @@
+
 class Objective < ActiveRecord::Base
+  include Geokit::Geocoders
 
   belongs_to :trip
-  acts_as_mappable
-  before_validation :geocode_location, :on => :create
+  # acts_as_mappable  :auto_geocode => true
+  after_create :get_location
 
-# All classes are called using the following signature:
 
-# include Geokit::Geocoders
-# location = XxxGeocoder.geocode(address)
+  def get_location
+    place = Geokit::Geocoders::GoogleGeocoder.geocode(to_addr)
+    self.update(lat: place.lat,lng: place.lng)
+  end
 
-  private
-
-    def geocode_location
-      geo=Geokit::Geocoders::MultiGeocoder.geocode (self.to_addr)
-      errors.add(:address, "Could not Geocode address") if !geo.success
-      self.lat, self.lng = geo.lat,geo.lng if geo.success
-    end
-
-    def to_addr
-      self.city + self.state + self.country
-    end
+  def to_addr
+    "#{self.city}" + ", #{self.state}" + ", #{self.country}"
+  end                  
 end
