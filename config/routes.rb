6946fac50f0f007f70b701f54d1777a7c10c07bd
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-09 12:50:11 -0400

    commented out unnecessary custom routes because new controller actions and view were implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   
+  # devise_for :users, :skip => [:sessions]
+  # as :user do
+  #   get 'signin' => 'devise/sessions#new', :as => :new_session
+  #   post 'signin' => 'devise/sessions#create', :as => :session
+  #   match 'signout' => 'devise/sessions#destroy', :as => :destroy_session,
+  #     :via => Devise.mappings[:user].sign_out_via
+  # end
+
   get 'users/:id', to: 'users#show', as: "user"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
