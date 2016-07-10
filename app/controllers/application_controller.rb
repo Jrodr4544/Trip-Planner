53db815ba8a43bfdf04b90a01b6f7bb6608af10c
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-10 14:35:09 -0400

    added strong parameters to allow name attribute for devise parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Adding this will allow strong parameters to devise_controller
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+    def configure_permitted_parameters
+    # allowing name parameter to devise users
+      devise_parameter_sanitizer.for(:sign_up) << :name
+    end
 end
 
