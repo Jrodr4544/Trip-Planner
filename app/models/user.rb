9216d5890a582916094453f853d172fec7c3b76e
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-10 10:56:22 -0400

    added set name function to users from omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
-         
+
   validates :name,  presence:   true
   validates :email, presence:   true
   validates :email, uniqueness: true
   has_many  :trips
   has_many  :objectives, through: :trips
 
+  def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.name     = auth.info.email.split('@').first
+        user.provider = auth.provider
+        user.uid      = auth.uid
+        user.email    = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+      end
+  end
 end
