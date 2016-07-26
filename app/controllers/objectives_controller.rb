2322e54a5a290316378b1440b1b0765e7d5168da
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-26 13:00:03 -0400

    added set_trip method in order to call objective based on trip

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,18 +1,21 @@
 class ObjectivesController < ApplicationController
-  before_action :set_objective, only: [:edit, :update, :destroy]
+  # before_action :set_objective, only: [:edit, :update, :destroy]
+  before_action :set_trip, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   def index
+    @objectives = @trip.objectives
   end
 
   def show
+    @objective = @trip.objectives.find(params[:id])
   end
 
   def new
-    @objective = Objective.new
+    @objective = @trip.objectives.build
   end
 
   def create
-     @objective = Objective.create(objective_params)
+    @objective = @trip.objectives.create(objective_params)
 
     if @objective.save
       redirect_to '/trips', notice: 'Objective was successfully created.'
@@ -22,9 +25,12 @@ class ObjectivesController < ApplicationController
   end
 
   def edit
+    @objective = @trip.objectives.find(params[:id])
   end
 
   def update
+    @objective = @trip.objectives.find(params[:id])
+
     if @objective.update(objective_params)
       redirect_to '/trips', notice: 'Objective was successfully updated.'
     else
@@ -33,12 +39,20 @@ class ObjectivesController < ApplicationController
   end
 
   def destroy
+    binding.pry
+    @objective = @trip.objective
+    @objective.destroy
+    redirect_to trips_path
   end
 
   private
 
-    def set_objective
-      @objective = Objective.find(params[:id])
+    def set_trip
+      if params[:trip_id]
+        @trip = Trip.find(params[:trip_id]) 
+      else
+        @trip = Objective.find(params[:id]).trip
+      end
     end
 
     def objective_params
