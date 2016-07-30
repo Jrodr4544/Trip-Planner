bc3ae1c1c843c44cb91677acefc8108319512a13
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-29 21:28:16 -0400

    added validations for location attributes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,8 @@
 class Location < ActiveRecord::Base
 has_many :objectives
 has_many :trips, through: :objectives
+validates :city,    presence: true 
+validates :country, presence: true
+validates :state,   presence: true
 
 end
