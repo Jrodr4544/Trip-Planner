ab4011ed179061b21dc84005da6ff8e325244379
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-29 21:30:02 -0400

    moved create_location method, to_addr method, and validations to Location model

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,23 +1,20 @@
 
 class Objective < ActiveRecord::Base
-  include Geokit::Geocoders
+  # include Geokit::Geocoders
   validates :title,   presence: true
-  validates :city,    presence: true 
-  validates :country, presence: true
-  validates :state,   presence: true
   belongs_to :trip
   belongs_to :location
 
-  after_create :create_location
+  # after_create :create_location
 
-  def create_location
-    place = Geokit::Geocoders::GoogleGeocoder.geocode(to_addr)
-    Location.create(lat: place.lat,lng: place.lng)
-    Location.last.save
-    self.update(location_id: Location.last.id)
-  end
+  # def create_location
+  #   place = Geokit::Geocoders::GoogleGeocoder.geocode(to_addr)
+  #   Location.create(lat: place.lat,lng: place.lng)
+  #   Location.last.save
+  #   self.update(location_id: Location.last.id)
+  # end
 
-  def to_addr
-    "#{self.city}" + ", #{self.state}" + ", #{self.country}"
-  end                  
+  # def to_addr
+  #   "#{self.city}" + ", #{self.state}" + ", #{self.country}"
+  # end                  
 end
