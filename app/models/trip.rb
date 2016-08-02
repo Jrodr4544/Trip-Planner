eadf362a98d9311581f9846834205809c3f3192b
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-08-02 13:30:56 -0400

    added method to find or create objective based on nested attributes

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,4 +5,11 @@ has_many    :locations, through: :objectives
 belongs_to  :user
 accepts_nested_attributes_for :objectives
 
+  def objectives_attributes=(objectives_attributes)
+    objectives_attributes.values.each do |objective_attribute|
+      objective = Objective.find_or_create_by(objective_attribute)
+      self.objectives << category
+    end
+  end
+
 end
