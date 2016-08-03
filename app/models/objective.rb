4c3adb23cf11f436f17c35f5c30bba78d89a9289
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-08-03 11:57:32 -0400

    removed geocode functionality from objective model

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,20 +1,6 @@
-
 class Objective < ActiveRecord::Base
-  # include Geokit::Geocoders
   validates :title,   presence: true
   belongs_to :trip
   belongs_to :location
-
-  # after_create :create_location
-
-  # def create_location
-  #   place = Geokit::Geocoders::GoogleGeocoder.geocode(to_addr)
-  #   Location.create(lat: place.lat,lng: place.lng)
-  #   Location.last.save
-  #   self.update(location_id: Location.last.id)
-  # end
-
-  # def to_addr
-  #   "#{self.city}" + ", #{self.state}" + ", #{self.country}"
-  # end                  
+             
 end
