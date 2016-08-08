0dff91584568f8f92f5d1818ac82d7fad722b6ce
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-08-08 08:58:54 -0400

    removed commented code and added comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # including omniauth callback controller for when a user signs in
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  
-  # devise_for :users, :skip => [:sessions]
-  # as :user do
-  #   get 'signin' => 'devise/sessions#new', :as => :new_session
-  #   post 'signin' => 'devise/sessions#create', :as => :session
-  #   match 'signout' => 'devise/sessions#destroy', :as => :destroy_session,
-  #     :via => Devise.mappings[:user].sign_out_via
-  # end
 
   get 'users/:id', to: 'users#show', as: "user"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#home"
   delete  '/trips/:trip_id/objectives/:id' => 'objectives#destroy', as: "kill_objective"
   
-
+  # Nesting resources for trip objectives
   resources :trips do  
     resources :objectives
   end
