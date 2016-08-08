b21b6a1bf2b19478dde55e274bd70f8bd3fbdce3
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-08-08 08:55:56 -0400

    added comment describing action

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,7 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-    def facebook
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in_and_redirect @user
-    end
+  # facebook callback when a user signs in
+  def facebook
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in_and_redirect @user
+  end
 end
