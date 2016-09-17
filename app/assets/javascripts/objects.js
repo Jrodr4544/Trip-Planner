0bb699512a90a9b5c677188c5b3bbcadcb512c29
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-09-17 14:21:11 -0400

    moved js script function from objectives/_form to objects.js so that the same event handler works for trips/show

--- app/assets/javascripts/objects.js
+++ app/assets/javascripts/objects.js
@@ -9,3 +9,23 @@ function Objective(id, title, notes) {
   this.title  = title;
   this.notes  = notes;
 }
+// The trips show does not like the event handler below for some reason
+$(function () {
+
+  $('.objectives.new').click(function(event) {
+  //prevent form from submitting the default way
+  event.preventDefault();
+
+  var values = $(this).serialize();
+  var trip_id = $('select#objective_trip_id').val();
+
+  // make sure that values is set properly, otherwise grab each individual value from the input fields. Maybe use the trip id field to send a post to /trips/:trip_id/objectives  
+  var posting = $.post('/trips/'+trip_id+'/objectives', values);
+  
+  posting.done(function(data){
+    debugger
+  })
+
+ });
+});
+
