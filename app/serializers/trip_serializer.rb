745d259a58985dc190f6ced4c453d0a9fc43270b
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-09-18 14:52:34 -0400

    added has_many objectives relationship to trip serializer

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,4 +1,5 @@
 class TripSerializer < ActiveModel::Serializer
   attributes :id, :title, :description
   # maybe nest objectives under here
+  has_many :objectives, serializer: ObjectiveSerializer
 end
