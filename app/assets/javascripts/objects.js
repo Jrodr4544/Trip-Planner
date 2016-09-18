345185672c6f77ceb1168e872094c35769a3f583
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-09-18 14:50:11 -0400

    changed the ajax request format, and correctly using form selector. Also creating js object on post response

--- app/assets/javascripts/objects.js
+++ app/assets/javascripts/objects.js
@@ -12,19 +12,31 @@ function Objective(id, title, notes) {
 // The trips show does not like the event handler below for some reason
 $(function () {
 
-  $('.objectives.new').click(function(event) {
+  $('form#new_objective').submit(function(event) {
   //prevent form from submitting the default way
   event.preventDefault();
-
-  var values = $(this).serialize();
+  debugger
+  var values  = $(this).serialize();
   var trip_id = $('select#objective_trip_id').val();
-
-  // make sure that values is set properly, otherwise grab each individual value from the input fields. Maybe use the trip id field to send a post to /trips/:trip_id/objectives  
-  var posting = $.post('/trips/'+trip_id+'/objectives', values);
+  var url     = '/trips/'+trip_id+'/objectives';
   
-  posting.done(function(data){
-    debugger
-  })
+  var posting = function(url, values){
+    $.ajax({
+      url: url,
+      type: 'POST',
+      dataType: 'json',
+      data: values
+    }).done( function(data) {
+      var objective = new Objective(data["id"], data["title"], data["notes"]);
+      // myGists(e.owner.login, token);
+    });
+  }
+  posting(url, values);
+  // posting.done(function(data){
+  //   debugger
+  //   
+  //   alert(data);
+  // })
 
  });
 });
