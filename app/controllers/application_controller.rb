249c02efe746d87030739cb915fc1f0a8afd57dc
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-10-08 09:34:23 -0400

    added 404 error rendering when resource not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::Base
   # Adding this will allow strong parameters to devise_controller
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # This will give 404 page when record is not found
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   # Redirecting to sign in after sign out
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path 
@@ -16,6 +19,10 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
     # allowing name parameter to devise users
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
+    def record_not_found
+      render :file => "#{Rails.root}/public/404"
     end
 end
 
